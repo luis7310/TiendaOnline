515c8d4e16c0e6d74b82e22e67dd3deb480777f9
luis7310 <layon@mundocuervo.com>
2024-10-17 18:29:14 -0600

    Validacion datos loggin

--- Front-end/JavaScript/index.js
+++ Front-end/JavaScript/index.js
@@ -3,7 +3,7 @@ var btn_login = document.getElementById("login-btn");
 
 //variables de interes
 const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //expresion reg validar correo
-
+const anchoPantalla = window.innerWidth; //ancho de pantalla
 
 //login pag
 var pagina_iniciar_se = document.getElementById("container-loggin-f"); // ventana iniciar sesion
@@ -11,9 +11,32 @@ var btn_inciar_s = document.getElementById("btn_login_page"); //botton inicia se
 var inp_mail = document.getElementById("input_login_mail"); //input correo 
 var inp_pass = document.getElementById("input_login_pass"); //input password
 var msg_inp = document.getElementById("msg-text-inp"); //cuadro de mensaje
+var barra_menu_s = document.getElementById("open-menu"); //barra del nav smarphone
+var botones = document.getElementsByClassName("btn-ds"); //botones de la barra de bav
+var btn_cancel_loggin = document.getElementById("btn-cancel"); //boton cancelar pagina iniciar sesion
+var btn_home = document.getElementsByClassName("home_btn"); //boton de home
+
+//boton home
+btn_home[0].addEventListener('click',()=>{
+    for(let i = 0; i < botones.length; i++){
+        botones[i].classList.remove('desactivado');
+    }
+    pagina_iniciar_se.classList.add('no-visible');
+})
+btn_home[1].addEventListener('click',()=>{
+    for(let i = 0; i < botones.length; i++){
+        botones[i].classList.remove('desactivado');
+    }
+    pagina_iniciar_se.classList.add('no-visible');
+})
+
 
 //mostrar ventana de login
 btn_login.addEventListener('click',()=>{
+    for(let i = 0; i < botones.length; i++){
+        botones[i].classList.add('desactivado');
+    }
+    
     msg_inp.innerText= "";
     inp_mail.classList.remove("error-input");
     inp_pass.classList.remove("error-input");
@@ -25,6 +48,20 @@ btn_login.addEventListener('click',()=>{
     else{
         pagina_iniciar_se.classList.add('no-visible');
     }
+    if(anchoPantalla<1000){
+        barra_menu_s.checked = !barra_menu_s.checked
+    }
+    else{
+
+    }
+})
+
+//cancelar inicio de sesion
+btn_cancel_loggin.addEventListener('click',()=>{
+    for(let i = 0; i < botones.length; i++){
+        botones[i].classList.remove('desactivado');
+    }
+    pagina_iniciar_se.classList.add('no-visible');
 })
 
 //validar datos de inicio de sesion
